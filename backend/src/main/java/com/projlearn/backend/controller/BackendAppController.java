da4c770498ab36e0fc793f617fafb4563b7f47e3
Russel Rai <leon.amorphy@googlemail.com>
2023-01-21 13:07:45 +0000

    update: change BackendAppController to dev environment

--- backend/src/main/java/com/projlearn/backend/controller/BackendAppController.java
+++ backend/src/main/java/com/projlearn/backend/controller/BackendAppController.java
@@ -1,8 +1,10 @@
 package com.projlearn.backend.controller;
 
+import org.springframework.context.annotation.Profile;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@Profile("dev")
 @RestController
 public class BackendAppController {
 
