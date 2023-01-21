2fb0f7991fc5c60586f46dfa422d0a3dd166e0fa
Russel Rai <leon.amorphy@googlemail.com>
2023-01-21 12:53:50 +0000

    feat: add initial HTTP req test

---
+++ backend/src/main/java/com/projlearn/backend/controller/BackendAppController.java
@@ -0,0 +1,14 @@
+package com.projlearn.backend.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class BackendAppController {
+
+  @RequestMapping("/")
+  public String helloWorld() {
+    return "Hello World from Spring Boot";
+  }
+
+}
