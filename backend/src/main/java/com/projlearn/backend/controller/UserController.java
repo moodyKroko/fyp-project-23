415bd7c5d50364ca21bf9320ea58be37e3e70fd2
Russel Rai <leon.amorphy@googlemail.com>
2023-01-24 00:56:45 +0000

    fix: change autowired to add constructor param

--- backend/src/main/java/com/projlearn/backend/controller/UserController.java
+++ backend/src/main/java/com/projlearn/backend/controller/UserController.java
@@ -3,7 +3,6 @@ package com.projlearn.backend.controller;
 import com.projlearn.backend.entity.User;
 import com.projlearn.backend.services.UserService;
 import java.util.List;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -14,8 +13,11 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api/v1/users")
 public class UserController {
 
-  @Autowired
-  UserService userService;
+  private final UserService userService;
+
+  public UserController(UserService userService) {
+    this.userService = userService;
+  }
 
   @GetMapping
   public ResponseEntity<List<User>> getAllUsers() {
