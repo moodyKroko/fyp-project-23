bf1e5a0c73dd5500e2351ea398bf9d2ce7f4881d
Russel Rai <leon.amorphy@googlemail.com>
2023-01-24 01:10:59 +0000

    update: add controller findById + TODO

--- backend/src/main/java/com/projlearn/backend/controller/UserController.java
+++ backend/src/main/java/com/projlearn/backend/controller/UserController.java
@@ -6,6 +6,7 @@ import java.util.List;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -22,6 +23,12 @@ public class UserController {
   @GetMapping
   public ResponseEntity<List<User>> getAllUsers() {
     return new ResponseEntity<>(userService.getAllUsers(), HttpStatus.OK);
+  }
+
+  @GetMapping("/{id}")
+  public ResponseEntity<User> getUserById(@PathVariable Long id) {
+    // TODO: change this to resource not found later
+    return ResponseEntity.ok(userService.getUserById(id));
   }
 
 }
