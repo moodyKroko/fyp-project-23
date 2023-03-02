7f331b00f0ccc5c00505874e91769e802b767464
Russel Rai <leon.amorphy@googlemail.com>
2023-03-02 17:03:15 +0000

    update: database config
    del: data.sql + schema.sql

---
+++ backend/src/main/java/com/projlearn/backend/config/DataSourceConfig.java
@@ -0,0 +1,19 @@
+package com.projlearn.backend.config;
+
+import javax.sql.DataSource;
+import org.springframework.boot.jdbc.DataSourceBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class DataSourceConfig {
+
+  @Bean
+  public DataSource getDataSource() {
+    DataSourceBuilder dataSourceBuilder = DataSourceBuilder.create();
+    dataSourceBuilder.username("SA");
+    dataSourceBuilder.password("");
+    return dataSourceBuilder.build();
+  }
+
+}
--- backend/src/main/resources/db/h2/data.sql
+++
@@ -1,2 +0,0 @@
-INSERT INTO users VALUES (default, 'leon', 'amorphy', 'test@gmail.com');
-INSERT INTO users values (default, 'jen', 'tai', 'jentai@gmail.com');
