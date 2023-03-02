05aae25408a90cea0ad9a2fff851b7b7460d458f
Russel Rai <leon.amorphy@googlemail.com>
2023-03-02 18:31:23 +0000

    Update UserControllerTest.java

--- backend/src/test/java/com/projlearn/backend/controller/UserControllerTest.java
+++ backend/src/test/java/com/projlearn/backend/controller/UserControllerTest.java
@@ -3,8 +3,10 @@ package com.projlearn.backend.controller;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
+import com.github.javafaker.Faker;
 import com.projlearn.backend.entity.User;
 import com.projlearn.backend.services.UserService;
+import jakarta.transaction.Transactional;
 import java.util.ArrayList;
 import java.util.List;
 import org.junit.jupiter.api.Test;
@@ -26,27 +28,34 @@ public class UserControllerTest {
   private UserService userService;
 
   @Test
-  public void TestGetAllUsers() throws Exception {
-
-    User user = new User();
-    user.setFirstName("Leon");
-    user.setLastName("Rai");
-    user.setEmail("test@gmail.com");
-
-    User user2 = new User();
-    user.setFirstName("Anna");
-    user.setLastName("Tee");
-    user.setEmail("test2@gmail.com");
+  public void shouldInsertUsers_thenFindAllUsers() throws Exception {
+    Faker faker = new Faker();
 
     List<User> users = new ArrayList<>();
-    users.add(user);
-    users.add(user2);
+
+    StringBuilder firstName = new StringBuilder();
+    StringBuilder lastName = new StringBuilder();
+    StringBuilder email = new StringBuilder();
+
+    int userCount = 3;
+    for (int i = 0; i < userCount; i++) {
+      firstName.append(faker.name().firstName());
+      lastName.append(faker.name().lastName());
+      email.append(String.format("%s.%s@gmail.com", firstName, lastName));
+
+      User newUser = new User();
+      newUser.setFirstName(firstName.toString());
+      newUser.setLastName(lastName.toString());
+      newUser.setEmail(email.toString());
+
+      users.add(newUser);
+    }
 
     Mockito.when(userService.getAllUsers()).thenReturn(users);
 
-    mvc.perform(get("/api/v1/users").contentType(MediaType.APPLICATION_JSON))
+    mvc.perform(get("/users").contentType(MediaType.APPLICATION_JSON))
         .andExpect(status().isOk())
-        .andExpect(MockMvcResultMatchers.jsonPath("$.length()").value(2));
+        .andExpect(MockMvcResultMatchers.jsonPath("$.length()").value(userCount));
 
   }
 
