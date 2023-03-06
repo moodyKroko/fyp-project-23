1cf6a6bfd83cdcbcd90b6af8cc23318726137b0a
Russel Rai <leon.amorphy@googlemail.com>
2023-03-06 15:45:03 +0000

    Put 404 page in correct directory

---
+++ frontend/pages/404.js
@@ -0,0 +1,12 @@
+import { Box, Container, Text } from '@chakra-ui/react'
+
+const NotFound = () => {
+  return (
+    <Box>
+      <Text>Ooooops.......</Text>
+      <Text>Page cannot be found</Text>
+    </Box>
+  )
+}
+
+export default NotFound
