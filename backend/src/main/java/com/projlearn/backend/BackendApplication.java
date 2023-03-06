a58e8b5450c5944b6ec169c36c8604a4a7d02d8c
Russel Rai <leon.amorphy@googlemail.com>
2023-03-06 21:09:54 +0000

    allow local:3000 as origin

--- backend/src/main/java/com/projlearn/backend/BackendApplication.java
+++ backend/src/main/java/com/projlearn/backend/BackendApplication.java
@@ -21,7 +21,7 @@ public class BackendApplication {
     return new WebMvcConfigurer() {
       @Override
       public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/*").allowedOrigins("http://localhost:8081");
+        registry.addMapping("/**").allowedOrigins("http://localhost:3000");
       }
     };
   }
