d7b86248db4223c7edafdcdc4b1c7d40fbdf0682
Russel Rai <leon.amorphy@googlemail.com>
2023-03-06 19:59:24 +0000

    update: allow cors local dev

--- backend/src/main/java/com/projlearn/backend/BackendApplication.java
+++ backend/src/main/java/com/projlearn/backend/BackendApplication.java
@@ -3,15 +3,26 @@ package com.projlearn.backend;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-//@EnableJpaRepositories // problem with controllerTest class
+// @EnableJpaRepositories // problem with controllerTest class
 @SpringBootApplication
 @EntityScan
 public class BackendApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(BackendApplication.class, args);
-	}
+  public static void main(String[] args) {
+    SpringApplication.run(BackendApplication.class, args);
+  }
 
+	@Bean
+  public WebMvcConfigurer corsConfigurer() {
+    return new WebMvcConfigurer() {
+      @Override
+      public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/*").allowedOrigins("http://localhost:8081");
+      }
+    };
+  }
 }
