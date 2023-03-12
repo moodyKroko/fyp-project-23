2ffaa29e0a818113a75c346e846b8ca0390c162a
Russel Rai <leon.amorphy@googlemail.com>
2023-03-12 18:09:52 +0000

    finished list of projects table

--- frontend/pages/projects/index.tsx
+++ frontend/pages/projects/index.tsx
@@ -1,8 +1,10 @@
 import {
   Box,
+  Container,
   Grid,
   GridItem,
   Link,
+  Spacer,
   Stack,
   Text,
   useColorModeValue
@@ -24,17 +26,23 @@ type Projects = {
 export const getStaticProps: GetStaticProps<{
   projects: Projects[]
 }> = async context => {
-  const res = await fetch('http://localhost:8081/projects')
-  const projects: Projects[] = await res.json()
+  try {
+    const res = await fetch('http://localhost:8081/projects')
+    const projects: Projects[] = await res.json()
 
-  if (!projects) {
+    if (!projects) {
+      return {
+        notFound: true
+      }
+    }
+
+    return {
+      props: { projects }
+    }
+  } catch (error) {
     return {
       notFound: true
     }
-  }
-
-  return {
-    props: { projects }
   }
 }
 
@@ -50,79 +58,78 @@ function Projects({ projects }) {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       {/* Table */}
-      <Stack
-        width={'full'}
-        // justifyContent={'center'}
-        direction={'column'}
-        spacing={4}
-        padding={4}
-        mt={24}
-      >
-        {/* Table header */}
-        <Grid
-          //   p={4}
-          justifyItems={'center'}
-          alignItems={'center'}
-          borderBottom={'2px solid gray'}
-          templateColumns="repeat(3, 1fr)"
+      <Container maxW={'container.lg'}>
+        <Stack
+          width={'full'}
+          // justifyContent={'center'}
+          direction={'column'}
+          spacing={4}
+          padding={4}
+          mt={0}
         >
-          <GridItem>
-            <Text fontSize={'xl'} p={4}>
-              Title
+          {/* Table header */}
+          <Grid
+            pl={16}
+            py={4}
+            pr={4}
+            // justifyItems={'center'}
+            alignItems={'center'}
+            borderBottom={'2px solid gray'}
+            templateColumns="repeat(4, 1fr)"
+          >
+            <GridItem colSpan={2}>
+              <Text fontSize={'xl'}>Title</Text>
+            </GridItem>
+            <GridItem>
+              <Text fontSize={'xl'}>Status</Text>
+            </GridItem>
+            <GridItem>
+              <Text fontSize={'xl'}>Difficulty</Text>
+            </GridItem>
+          </Grid>
+          {/* Table content */}
+          <Box border="1px solid gray" borderRadius={'xl'}>
+            <Text pl={16} py={4} borderBottom={'1px solid grey'}>
+              Showing {projects.length} results
             </Text>
-          </GridItem>
-          <GridItem>
-            <Text fontSize={'xl'}>Status</Text>
-          </GridItem>
-          <GridItem>
-            <Text fontSize={'xl'}>Difficulty</Text>
-          </GridItem>
-        </Grid>
-        {/* Table content */}
-        <Box border="1px solid gray" borderRadius={'xl'}>
-          {/* <Text>Showing {projects.length} results</Text> */}
-          {projects.map((project: Projects) => (
-            <ProjectItems key={project.id} project={project} />
-          ))}
-        </Box>
-      </Stack>
+            {projects.map((project: Projects) => (
+              <ProjectItems key={project.id} project={project} />
+            ))}
+          </Box>
+        </Stack>
+      </Container>
     </Box>
   )
 }
 
 function ProjectItems({ project }) {
-  const textHighlightColor = useColorModeValue('purple.600', 'purple.500')
-  const bgHighlight = useColorModeValue('#d9d9d9', '#313a4e')
+  const textHighlightColor = useColorModeValue('blue.500', 'purple.500')
+  const bgHighlight = useColorModeValue('#f3eaff', '#44526d')
 
   return (
     <Box
+      pl={16}
+      py={4}
+      pr={4}
+      as={motion.div}
       borderBottom={'1px solid slategray'}
-      _hover={{ backgroundColor: bgHighlight }}
+      whileHover={{ backgroundColor: bgHighlight }}
       _last={{ borderBottom: 'none', borderBottomRadius: 'xl' }}
-      _first={{ borderTopRadius: 'xl' }}
     >
-      <Box as={motion.div} whileHover={{ scale: 1.1 }} transition="linear 0.1s">
-        <Grid
-          //   justifyItems={'left'}
-          p={8}
-          alignItems={'center'}
-          templateColumns="repeat(3, 1fr)"
-          _hover={{ color: textHighlightColor }}
-        >
-          <GridItem colSpan={1}>
-            <Link as={NextLink} href={`/projects/${project.id}`}>
-              <Box textAlign={'center'}>{project.title}</Box>
-            </Link>
-          </GridItem>
+      <Grid templateColumns="repeat(4, 1fr)">
+        <GridItem colSpan={2}>
+          <Link
+            as={NextLink}
+            href={`/projects/${project.id}`}
+            _hover={{ color: textHighlightColor }}
+          >
+            {project.title}
+          </Link>
+        </GridItem>
 
-          <GridItem colSpan={1}>
-            <Box textAlign={'center'}>{project.status}</Box>
-          </GridItem>
-          <GridItem colSpan={1}>
-            <Box textAlign={'center'}>{project.difficulty}</Box>
-          </GridItem>
-        </Grid>
-      </Box>
+        <GridItem>{project.status}</GridItem>
+        <GridItem>{project.difficulty}</GridItem>
+      </Grid>
     </Box>
   )
 }
